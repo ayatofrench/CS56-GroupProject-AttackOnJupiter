4825859916110ca2b6541ed38a530d5dc14836ec
Donovanphenry <48369383+Donovanphenry@users.noreply.github.com>
2019-05-15 10:26:18 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
-package application;
+package AoJGame;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-
+public class Main extends Application 
+{
     private static Stage stage;
     
     public static void main(String [] args)
@@ -13,11 +13,10 @@ public class Main extends Application {
     	System.exit(0);
     }
 
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) 
+    {
         stage = primaryStage;
         GameEngine engine = new GameEngine();
-
         stage.setTitle("Game Test");
         stage.setFullScreen(true);
         stage.setResizable(false);
@@ -25,7 +24,8 @@ public class Main extends Application {
         stage.show();
     }
 
-    public static Stage getStage() {
+    public static Stage getStage() 
+    {
         return stage;
     }
 }
