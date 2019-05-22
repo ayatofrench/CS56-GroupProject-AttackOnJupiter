f7e9986d6b87e407f3a827e00cb50e90b08c417a
Donovanphenry <48369383+Donovanphenry@users.noreply.github.com>
2019-05-22 15:46:15 -0700

    Update NPC.java

--- NPC.java
+++ NPC.java
@@ -14,19 +14,27 @@ public class NPC extends Character
 	private int time;
 	private int xFinal;
 	private boolean moving = false;
-	private Image metaManLeft = new Image("file:Pictures/metaman_left.gif");
+	private Image metaManLeft = new Image("file:Pictures/metaman/metaman_left.gif");
     private ImagePattern metaManLeftP = new ImagePattern(metaManLeft);
-    private Image metaManRight = new Image("file:Pictures/metaman_left.gif");
+    private Image metaManRight = new Image("file:Pictures/metaman/metaman_left.gif");
     private ImagePattern metaManRightP = new ImagePattern(metaManRight);
-    private Image worm_right = new Image("file:Pictures/worm_right.gif");
+    private Image gunslingerLeft = new Image("file:Pictures/lisa_range/lisa_range_left.gif");
+    private ImagePattern gunslingerLeftP = new ImagePattern(gunslingerLeft);
+    private Image gunslingerRight = new Image("file:Pictures/lisa_range/lisa_range_right.gif");
+    private ImagePattern gunslingerRightP = new ImagePattern(gunslingerRight);
+    private Image worm_right = new Image("file:Pictures/metaman/worm_right.gif");
     private ImagePattern wormRightP = new ImagePattern(worm_right);
-    private Image worm_left = new Image("file:Pictures/worm_left.gif");
+    private Image worm_left = new Image("file:Pictures/metaman/worm_left.gif");
     private ImagePattern wormLeftP = new ImagePattern(worm_left);
+    private int bossLevel = 0;
     
     public NPC()
     {
     	this.sleepTime = 0;
-    	this.setFill(metaManLeftP);
+    	if (bossLevel == 1)
+				this.setFill(gunslingerLeftP);
+		if (bossLevel == 2)
+				this.setFill(gunslingerLeftP);
     	this.setHealth(120);
 		this.setWidth((int)(primaryScreenBounds.getWidth() * .2));
 		maxPosition = (int)(primaryScreenBounds.getWidth() - this.getWidth());
@@ -47,7 +55,7 @@ public class NPC extends Character
 		if (!this.isAlive())
 		{
 			game.getGamePane().getChildren().remove(proj);
-			game.initGame();
+			game.initGameTwo();
 		}
 		
 		if (this.isAlive())
@@ -78,7 +86,7 @@ public class NPC extends Character
 	        			proj.getHeight()).intersects(getBoundary(this.getEnemy().getX(), this.getEnemy().getY(), 
 	        					this.getEnemy().getWidth(), this.getEnemy().getHeight())))
 					{
-		        		this.getEnemy().reduceHealth(10);
+		        		this.getEnemy().reduceHealth(120);
 		        		if (!this.getEnemy().isAlive())
 		        		{
 		        			game.getGamePane().getChildren().remove(this.getEnemy());
@@ -116,12 +124,18 @@ public class NPC extends Character
 	     		if (this.getDirection().equalsIgnoreCase("right"))
 	     		{
 	     			this.setX(this.getX() + this.getMovementSpeed() * time);
-	     			this.setFill(metaManRightP);
+	     			if (bossLevel == 1)
+	     				this.setFill(metaManRightP);
+	     			if(bossLevel == 2)
+	     				this.setFill(gunslingerRightP);
 	     		}
 	     		if (this.getDirection().equalsIgnoreCase("left"))
 	     		{
-	     			this.setFill(metaManLeftP);
-	     			this.setX(this.getX() - this.getMovementSpeed() * time);
+	     			if (bossLevel == 1)
+	     				this.setFill(metaManLeftP);
+	     			if (bossLevel == 2)
+	     				this.setFill(gunslingerLeftP);
+	     				this.setX(this.getX() - this.getMovementSpeed() * time);
 	     		}
 	     		
 	     		if (xFinal == this.getX())
@@ -131,7 +145,10 @@ public class NPC extends Character
 	     				xFinal = (int)(Math.random()*((maxPosition  + 1)));
 	     				this.setDirection("left");
 	     				checkXFinal(this);
-	         			this.setFill(metaManLeftP);
+	     				if (bossLevel == 1)
+	     					this.setFill(metaManRightP);
+	     				if (bossLevel == 2)
+	     					this.setFill(gunslingerLeftP);
 	         		}
 	         		
 	     			else if (this.getDirection().equalsIgnoreCase("left"))
@@ -139,7 +156,10 @@ public class NPC extends Character
 	     				xFinal = (int)(Math.random()*((maxPosition  + 1)));
 	         			this.setDirection("right");
 	         			checkXFinal(this);
-	         			this.setFill(metaManRightP);
+	         			if (bossLevel == 1)
+	         				this.setFill(metaManRightP);
+	         			if (bossLevel == 2)
+	         				this.setFill(gunslingerRightP);
 	         		}
 	     		}
 	     	}
@@ -158,10 +178,20 @@ public class NPC extends Character
 	            	jumping = false;
 	               
 	            	if (this.getDirection().equalsIgnoreCase("left"))
-	            		this.setFill(metaManLeftP);
+	            	{
+	            		if (bossLevel == 1)
+		     				this.setFill(metaManLeftP);
+		     			if (bossLevel == 2)
+		     				this.setFill(gunslingerLeftP);
+	            	}
 	               
 	            	if (this.getDirection().equalsIgnoreCase("right"))
-	            		this.setFill(metaManRightP);
+	            	{
+	            		if (bossLevel == 1)
+	         				this.setFill(metaManRightP);
+	         			if (bossLevel == 2)
+	         				this.setFill(gunslingerRightP);
+	            	}
 	            }
 	        }
 			
@@ -171,7 +201,10 @@ public class NPC extends Character
 	    		this.setX(0);    
 	    		xFinal = (int)(Math.random()*( (maxPosition + 1) ) );
 	    		checkXFinal(this);
-	    		this.setFill(metaManRightP);
+	    		if (bossLevel == 1)
+     				this.setFill(metaManRightP);
+     			if (bossLevel == 2)
+     				this.setFill(gunslingerRightP);
 	        }
 			
 			if (this.getX() > maxPosition)
@@ -180,7 +213,10 @@ public class NPC extends Character
 	    		this.setX(maxPosition);
 	    		xFinal = (int)(Math.random()*((maxPosition + 1)));
 	    		checkXFinal(this);
-	    		this.setFill(metaManLeftP);
+	    		if (bossLevel == 1)
+     				this.setFill(metaManLeftP);
+     			if (bossLevel == 2)
+     				this.setFill(gunslingerLeftP);
 	    	}
 			
 			if (this.getY() < 0)
@@ -212,5 +248,15 @@ public class NPC extends Character
 		proj = new Projectile(this.getX() + this.getWidth(), this.getY() + (this.getHeight() / 2), this.direction);
 		game.getGamePane().getChildren().add(proj);
 		shooting = true;
+	}
+	
+	public void setBossLevel(int bossLevel)
+	{
+		this.bossLevel = bossLevel;
+	}
+	
+	public int getBossLevel()
+	{
+		return this.bossLevel;
 	}
 }
